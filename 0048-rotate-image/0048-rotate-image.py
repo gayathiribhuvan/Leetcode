211d5f483c6f55908411b7ecd9099ef9fcfaa8d5
gayathiribhuvan <gayathiribhuvaneshwaran@gmail.com>
2026-03-13 20:21:15 +0530

    Time: 0 ms (100%), Space: 19.2 MB (89.52%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,10 +1,11 @@
-class Solution(object):
-    def rotate(self, matrix):
-        #Rotate image to 90 degrees
-        n = len(matrix)
-        for i in range(n):
-            for j in range(i+1,n):
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        r = len(matrix)
+        c = len(matrix[0])
+
+        for i in range(0,r):
+            for j in range(i+1,c):
                 matrix[i][j], matrix[j][i] = matrix[j][i],matrix[i][j]
 
-        for i in range(n):
+        for i in range(r):
             matrix[i] = matrix[i][::-1]
